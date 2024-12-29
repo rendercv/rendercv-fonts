a09d76ff1dbbbaef8a57146b5eac865f638428c1
Sina Atalay <dev@atalay.biz>
2024-12-29 05:42:49 -0500

    push fonts

--- rendercv_fonts/__init__.py
+++ rendercv_fonts/__init__.py
@@ -6,7 +6,14 @@ import pathlib
 
 __version__ = "0.1.0"
 
-available_font_families = ["SourceSans3"]
+available_font_families = [
+    "SourceSans3",
+    "Ubuntu",
+    "Roboto",
+    "OpenSans",
+    "NotoSans",
+    "Mukta",
+]
 path_of = {
     font_famiy: pathlib.Path(__file__).parent / font_famiy
     for font_famiy in available_font_families
