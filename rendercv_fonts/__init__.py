beebd70b764829a9e27c8bc17c0952b2452d4cd6
Sina Atalay <dev@atalay.biz>
2025-01-07 13:50:29 +0300

    add XCharter

--- rendercv_fonts/__init__.py
+++ rendercv_fonts/__init__.py
@@ -6,19 +6,16 @@ import pathlib
 
 __version__ = "0.1.0"
 
+package_folder_path = pathlib.Path(__file__).parent
+
+# Make a list of all the folders (except __pycache__) in the package folder:
 available_font_families = [
-    "SourceSans3",
-    "Ubuntu",
-    "Roboto",
-    "OpenSans",
-    "NotoSans",
-    "Mukta",
+    folder.name
+    for folder in package_folder_path.iterdir()
+    if folder.is_dir() and folder.name != "__pycache__"
 ]
 path_of = {
-    font_famiy: pathlib.Path(__file__).parent / font_famiy
+    font_famiy: package_folder_path / font_famiy
     for font_famiy in available_font_families
 }
-paths_to_font_folders = [
-    pathlib.Path(__file__).parent / font_family
-    for font_family in available_font_families
-]
+paths_to_font_folders = [path for path in path_of.values()]
